233b8bfbfbf464804a7112b5a79529169b3e307c
sbalci <drserdarbalci@gmail.com>
2022-04-03 21:48:04 +0300

    update

--- R/benford.h.R
+++ R/benford.h.R
@@ -46,7 +46,8 @@ benfordResults <- if (requireNamespace("jmvcore", quietly=TRUE)) R6::R6Class(
                 name="",
                 title="Benford Analysis",
                 refs=list(
-                    "benford.analysis"))
+                    "benford.analysis",
+                    "ClinicoPathJamoviModule"))
             self$add(jmvcore::Html$new(
                 options=options,
                 name="todo",
